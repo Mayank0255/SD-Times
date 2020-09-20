f2db3cd1210b93ae98f485ee19e0b5074cb9cee3
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-20 16:53:05 +0530

    feat: add fetchTopIds on display with spinner

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -5,11 +5,35 @@ class NewsList extends StatelessWidget {
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
 
+
+    // Temporary
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news here!'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>>snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
