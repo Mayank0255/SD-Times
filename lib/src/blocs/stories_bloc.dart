259dda87fcc6a6e7abe7ef9536d6f07d89fc9927
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-20 16:27:22 +0530

    feat: wire-up StoriesProvider and StoriesBloc

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,19 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+  final _repository = Repository();
+  final _topIds = PublishSubject<List<int>>();
+
+  Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
+
+  dispose() {
+    _topIds.close();
+  }
+}
