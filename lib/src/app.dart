5cc776865c8024d1f3d13835e5d9431a8bfaebab
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-29 19:08:23 +0530

    feat: NewsDetail screen boilerplate

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -23,7 +23,11 @@ class App extends StatelessWidget {
     } else {
       return MaterialPageRoute(
         builder: (context) {
-          return NewsDetail();
+          final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+          return NewsDetail(
+            itemId: itemId
+          );
         }
       );
     }
--- lib/src/screens/news_details.dart
+++ lib/src/screens/news_details.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  NewsDetail({this.itemId});
+
   Widget build(context) {
-    return Text('HEHEHEHEHEHE');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: Text('$itemId')
+    );
   }
 }
