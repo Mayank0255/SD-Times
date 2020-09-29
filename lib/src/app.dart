9774e34232be709f44c202d53e2535c8d98ec4bb
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-29 17:13:04 +0530

    feat: NewsDetail screen setup

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
+import 'screens/news_details.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
     return StoriesProvider(
       child: MaterialApp(
         title: 'News!',
-        home: NewsList(),
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        }
+      );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsDetail();
+        }
+      );
+    }
   }
 }
---
+++ lib/src/screens/news_details.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class NewsDetail extends StatelessWidget {
+  Widget build(context) {
+    return Text('HEHEHEHEHEHE');
+  }
+}
