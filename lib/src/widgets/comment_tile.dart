ae3ab9889f154fdbaae56f7d1166bfcb49af1adf
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-30 17:20:41 +0530

    feat: recursive comment rendering

--- lib/src/widgets/comment_tile.dart
+++ lib/src/widgets/comment_tile.dart
@@ -3,5 +3,33 @@ import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 
 class Comment extends StatelessWidget {
+  final int itemId;
+  final Map<int, Future<ItemModel>> itemMap;
+
+  Comment({this.itemId, this.itemMap});
   
+  Widget build(context) {
+    return FutureBuilder(
+      future: itemMap[itemId],
+      builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still loading');
+        }
+
+        final children = <Widget>[
+          Text(snapshot.data.text),
+        ];
+
+        snapshot.data.kids.forEach((kidId) {
+          children.add(
+            Comment(itemId: kidId, itemMap: itemMap),
+          );
+        });
+
+        return Column(
+          children: children,
+        );
+      },
+    );
+  }
 }
