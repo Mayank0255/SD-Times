9453203f8628774f5eaa19ea491c117fbd9abf22
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-30 13:06:05 +0530

    feat: wire-up comments provider and bloc to the screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
+import 'blocs/comments_provider.dart';
 import 'screens/news_details.dart';
+import 'screens/news_list.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        onGenerateRoute: routes,
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News!',
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
@@ -23,7 +26,10 @@ class App extends StatelessWidget {
     } else {
       return MaterialPageRoute(
         builder: (context) {
+          final commentsBloc = CommentsProvider.of(context);
           final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+          commentsBloc.fetchItemWithComments(itemId);
 
           return NewsDetail(
             itemId: itemId
