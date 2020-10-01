6fdef36f17e2b03cd5e38b866820377f743bdc5d
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-10-01 17:36:18 +0530

    style: TabBar ui changed

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -6,21 +6,46 @@ class NewsList extends StatelessWidget {
     return DefaultTabController(
       length: 3, 
       child: Scaffold(
-
         appBar: AppBar(
           title: Text('Top News'),
-          bottom: TabBar(tabs: [
-            Tab(text: 'Newest',),
-            Tab(text: 'Best',),
-            Tab(text: 'Top',),
-          ],),
+          elevation: 0,
+          bottom: TabBar(
+            unselectedLabelColor: Color.fromRGBO(98, 110, 227, 1.0),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              color: Color.fromRGBO(98, 110, 227, 1.0)
+            ),
+            tabs: [
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Best'),
+                ),
+              ),
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Top'),
+                ),
+              ),
+              Tab(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Newest'),
+                ),
+              ),
+            ],),
           backgroundColor: Color.fromRGBO(21, 49, 75, 1.0),
           shadowColor: Colors.black,
         ),
         body: TabBarView(children: [
-          NewsTab(listType: 'newest'),
           NewsTab(listType: 'best'),
           NewsTab(listType: 'top'),
+          NewsTab(listType: 'newest'),
         ],),
         backgroundColor: Color.fromRGBO(0, 21, 40, 1.0),
       ),
